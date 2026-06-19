d20a99d08a8f325d345795d282848c7e4bd8bac8
muawiya1 <muawiyaabk@gmail.com>
2026-06-19 21:57:54 +0100

    Fixed exercises

--- src/lib.rs
+++ src/lib.rs
@@ -24,6 +24,7 @@ pub struct Point<T> {
 impl<T> Point<T> {
     pub fn new(x: T, y: T) -> Self {
         // TODO: Implement constructor for Point
+        Point { x, y }
     }
 }
 
@@ -31,6 +32,7 @@ impl<T> Point<T> {
 pub trait BitcoinSerialize {
     fn serialize(&self) -> Vec<u8> {
         // TODO: Implement serialization to bytes
+        Vec::new()
     }
 }
 
@@ -46,6 +48,7 @@ pub struct LegacyTransaction {
 impl LegacyTransaction {
     pub fn builder() -> LegacyTransactionBuilder {
         // TODO: Return a new builder for constructing a transaction
+        LegacyTransactionBuilder::new()
     }
 }
 
@@ -60,32 +63,53 @@ pub struct LegacyTransactionBuilder {
 impl Default for LegacyTransactionBuilder {
     fn default() -> Self {
         // TODO: Implement default values
+        LegacyTransactionBuilder {
+            version: 1,
+            inputs: Vec::new(),
+            outputs: Vec::new(),
+            lock_time: 0,
+        }
     }
 }
 
 impl LegacyTransactionBuilder {
     pub fn new() -> Self {
         // TODO: Initialize new builder by calling default
+        Self::default()
     }
 
     pub fn version(mut self, version: i32) -> Self {
         // TODO: Set the transaction version
+        self.version = version;
+        self
     }
 
     pub fn add_input(mut self, input: TxInput) -> Self {
         // TODO: Add input to the transaction
+        self.inputs.push(input);
+        self
     }
 
     pub fn add_output(mut self, output: TxOutput) -> Self {
         // TODO: Add output to the transaction
+        self.outputs.push(output);
+        self
     }
 
     pub fn lock_time(mut self, lock_time: u32) -> Self {
         // TODO: Set lock_time for transaction
+        self.lock_time = lock_time;
+        self
     }
 
     pub fn build(self) -> LegacyTransaction {
         // TODO: Build and return the final LegacyTransaction
+        LegacyTransaction {
+            version: self.version,
+            inputs: self.inputs,
+            outputs: self.outputs,
+            lock_time: self.lock_time,
+        }
     }
 }
 
@@ -112,6 +136,16 @@ pub struct OutPoint {
 // Simple CLI argument parser
 pub fn parse_cli_args(args: &[String]) -> Result<CliCommand, BitcoinError> {
     // TODO: Match args to "send" or "balance" commands and parse required arguments
+    match args {
+        [_send, amount, address] => {
+            let amount = u64::from_str(amount).unwrap();
+            Ok(CliCommand::Send {
+                amount,
+                address: address.clone(),
+            })
+        }
+        _ => Err(BitcoinError::ParseError("Command is invalid".to_string())),
+    }
 }
 
 pub enum CliCommand {
@@ -126,6 +160,22 @@ impl TryFrom<&[u8]> for LegacyTransaction {
     fn try_from(data: &[u8]) -> Result<Self, Self::Error> {
         // TODO: Parse binary data into a LegacyTransaction
         // Minimum length is 10 bytes (4 version + 4 inputs count + 4 lock_time)
+        if data.len() < 10 {
+            return Err(BitcoinError::InvalidTransaction);
+        }
+        let version = i32::from_le_bytes(data[0..4].try_into().unwrap());
+        let inputs_count = u32::from_le_bytes(data[4..8].try_into().unwrap());
+        let outputs_count = u32::from_le_bytes(data[8..12].try_into().unwrap());
+        let lock_time = u32::from_le_bytes(data[12..16].try_into().unwrap());
+        let inputs = Vec::with_capacity(inputs_count as usize);
+        let outputs = Vec::with_capacity(outputs_count as usize);
+
+        Ok(LegacyTransaction {
+            version,
+            inputs,
+            outputs,
+            lock_time,
+        })
     }
 }
 
@@ -133,5 +183,12 @@ impl TryFrom<&[u8]> for LegacyTransaction {
 impl BitcoinSerialize for LegacyTransaction {
     fn serialize(&self) -> Vec<u8> {
         // TODO: Serialize only version and lock_time (simplified)
+        let mut result = Vec::new();
+
+        result.extend_from_slice(&self.version.to_le_bytes());
+
+        result.extend_from_slice(&self.lock_time.to_le_bytes());
+
+        result
     }
 }
